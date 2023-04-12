891d200c7bc7500dc214c20da05b0614d5fd9965
Imad <elbouhatiimad@gmail.com>
2023-04-12 05:57:13 +0000

    first commit

---
+++ app/src/main/java/com/smarttoolfactory/bubblelayout/BubbleLayout.kt
@@ -0,0 +1,397 @@
+package com.smarttoolfactory.bubblelayout
+
+import android.content.Context
+import android.graphics.*
+import android.os.Build
+import android.util.AttributeSet
+import android.view.View
+import android.view.ViewOutlineProvider
+import android.widget.FrameLayout
+import kotlin.math.min
+
+/**
+ * Linear layout that draws chat or speech bubble with specified properties.
+ *
+ * Properties are encapsulated inside [BubbleModifier]
+ */
+class BubbleLayout : FrameLayout {
+
+    lateinit var modifier: BubbleModifier
+
+    private val paint by lazy {
+        Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            style = Paint.Style.FILL
+            color = modifier.backgroundColor
+        }
+    }
+
+    private val paintDebug by lazy {
+        Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            style = Paint.Style.STROKE
+            strokeWidth = 4f
+            color = Color.RED
+        }
+    }
+
+    /**
+     * Rectangle for drawing content, this is the are that contains child views. Arrow is
+     * excluded from this rectangle.
+     */
+    private var rectContent = RectF()
+
+
+    /**
+     * Rectangle that covers content and arrow of the bubble layout. Total area is covered
+     * in this rectangle.
+     */
+    private var rectBubble = RectF()
+
+    private var path = Path()
+
+    /**
+     * Setting this flag to true draws content and bubble rectangles around this layout
+     */
+    var isDebug = false
+
+    constructor(context: Context) : super(context) {
+        modifier = BubbleModifier()
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.BubbleLayout)
+
+        modifier = BubbleModifier()
+        init()
+
+        modifier.backgroundColor = typedArray.getColor(
+            R.styleable.BubbleLayout_android_background,
+            modifier.backgroundColor
+        )
+        modifier.cornerRadius =
+            typedArray.getDimension(
+                R.styleable.BubbleLayout_cornerRadius,
+                modifier.cornerRadius
+            )
+
+        modifier.cornerRadiusBundle.topLeft = typedArray.getDimension(
+            R.styleable.BubbleLayout_cornerRadiusTopLeft,
+            modifier.cornerRadius
+        )
+
+        modifier.cornerRadiusBundle.topRight = typedArray.getDimension(
+            R.styleable.BubbleLayout_cornerRadiusTopRight,
+            modifier.cornerRadius
+        )
+
+        modifier.cornerRadiusBundle.bottomRight = typedArray.getDimension(
+            R.styleable.BubbleLayout_cornerRadiusBottomRight,
+            modifier.cornerRadius
+        )
+
+        modifier.cornerRadiusBundle.bottomLeft = typedArray.getDimension(
+            R.styleable.BubbleLayout_cornerRadiusBottomLeft,
+            modifier.cornerRadius
+        )
+
+        // Arrow
+        modifier.arrowAlignment = typedArray.getInt(R.styleable.BubbleLayout_arrowAlignment, NONE)
+
+        modifier.arrowWidth =
+            typedArray.getDimension(R.styleable.BubbleLayout_arrowWidth, modifier.arrowWidth)
+        modifier.arrowHeight =
+            typedArray.getDimension(R.styleable.BubbleLayout_arrowHeight, modifier.arrowHeight)
+        modifier.arrowRadius =
+            typedArray.getDimension(R.styleable.BubbleLayout_arrowRadius, modifier.arrowRadius)
+
+        val arrowShape =
+            typedArray.getInt(R.styleable.BubbleLayout_arrowShape, modifier.arrowShape.ordinal)
+
+        modifier.arrowShape = when (arrowShape) {
+            0 -> ArrowShape.TRIANGLE_RIGHT
+            1 -> ArrowShape.TRIANGLE_ISOSCELES
+            else -> ArrowShape.CURVED
+        }
+        modifier.arrowOffsetY =
+            typedArray.getDimension(R.styleable.BubbleLayout_arrowOffsetY, modifier.arrowOffsetY)
+        modifier.arrowOffsetX =
+            typedArray.getDimension(R.styleable.BubbleLayout_arrowOffsetY, modifier.arrowOffsetX)
+        modifier.withArrow =
+            typedArray.getBoolean(R.styleable.BubbleLayout_withArrow, modifier.withArrow)
+
+        // Elevation/Shadow
+        val shadowStyle =
+            typedArray.getInt(R.styleable.BubbleLayout_shadowStyle, modifier.shadowStyle.ordinal)
+
+        modifier.shadowStyle = if (shadowStyle == ShadowStyle.ELEVATION.ordinal) {
+            ShadowStyle.ELEVATION
+        } else ShadowStyle.SHADOW
+
+
+        if (modifier.shadowStyle == ShadowStyle.SHADOW) {
+            modifier.shadowColor =
+                typedArray.getColor(
+                    R.styleable.BubbleLayout_android_shadowColor,
+                    modifier.shadowColor
+                )
+            modifier.shadowRadius = typedArray.getFloat(
+                R.styleable.BubbleLayout_android_shadowRadius,
+                modifier.shadowRadius
+            )
+            modifier.shadowRadius = typedArray.getFloat(
+                R.styleable.BubbleLayout_android_shadowDx,
+                modifier.shadowOffsetX
+            )
+            modifier.shadowRadius = typedArray.getFloat(
+                R.styleable.BubbleLayout_android_shadowDy,
+                modifier.shadowOffsetY
+            )
+        }
+
+        typedArray.recycle()
+
+    }
+
+
+    private fun init() {
+
+        background = null
+
+        modifier.dp = dp(1f)
+
+        modifier.init()
+
+        if (modifier.shadowStyle == ShadowStyle.SHADOW) {
+
+            setLayerType(LAYER_TYPE_SOFTWARE, paint)
+            paint.setShadowLayer(
+                modifier.shadowRadius,
+                modifier.shadowOffsetX,
+                modifier.shadowOffsetY,
+                modifier.shadowColor
+            )
+        }
+
+        setWillNotDraw(false)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+
+        val pair = measureBubbleLayout(widthMeasureSpec, heightMeasureSpec)
+
+        val desiredWidth = pair.first
+        val desiredHeight: Int = pair.second
+
+        logMeasureSpecs("LOG: ", widthMeasureSpec, heightMeasureSpec)
+        setMeasuredDimension(desiredWidth, desiredHeight)
+    }
+
+    private fun measureBubbleLayout(
+        widthMeasureSpec: Int,
+        heightMeasureSpec: Int
+    ): Pair<Int, Int> {
+
+        // Set rectangle for content area, arrow is excluded, on
+        val alignment = modifier.arrowAlignment
+
+        val isHorizontalRightAligned = isHorizontalRightAligned(alignment)
+        val isHorizontalLeftAligned = isHorizontalLeftAligned(alignment)
+        val isVerticalBottomAligned = isVerticalBottomAligned(alignment)
+
+
+        // Measure dimensions
+        val widthMode = MeasureSpec.getMode(widthMeasureSpec)
+        val widthSize = MeasureSpec.getSize(widthMeasureSpec)
+        val heightMode = MeasureSpec.getMode(heightMeasureSpec)
+        val heightSize = MeasureSpec.getSize(heightMeasureSpec)
+
+
+        println("🎃 onMeasures widthSize: $widthSize, heightSize: $heightSize childCount: $childCount")
+
+        var maxContentWidth: Int = 0
+        var maxContentHeight: Int = 0
+
+        // Measure children to get width and height for content area
+        for (i in 0..childCount) {
+            val child: View? = getChildAt(i)
+            child?.measure(widthMeasureSpec, heightMeasureSpec)
+            child?.let { child ->
+                println(
+                    "🔥 onMeasure child #$i, " +
+                            "measuredWidth: ${child.measuredWidth}, width: ${child.width}, " +
+                            "measuredHeight: ${child.measuredHeight}, height: ${child.height}"
+                )
+
+                if (child.measuredWidth > maxContentWidth) maxContentWidth = child.measuredWidth
+                maxContentHeight += child.measuredHeight
+            }
+        }
+
+        var desiredWidth = resolveSize(
+            maxContentWidth,
+            widthMeasureSpec
+        ) + paddingStart + paddingEnd
+
+        if (isHorizontalLeftAligned || isHorizontalRightAligned) {
+            desiredWidth += modifier.arrowWidth.toInt()
+        }
+
+        var desiredHeight: Int =
+            resolveSize(maxContentHeight, heightMeasureSpec) + paddingTop + paddingBottom
+        if (isVerticalBottomAligned) desiredHeight += modifier.arrowHeight.toInt()
+
+        when {
+            isHorizontalLeftAligned -> {
+                rectContent.set(
+                    modifier.arrowWidth,
+                    0f,
+                    maxContentWidth.toFloat() + modifier.arrowWidth + paddingStart + paddingEnd,
+                    maxContentHeight.toFloat() + paddingTop + paddingBottom
+                )
+
+            }
+
+            isHorizontalRightAligned -> {
+                rectContent.set(
+                    0f,
+                    0f,
+                    maxContentWidth.toFloat() + paddingStart + paddingEnd,
+                    maxContentHeight.toFloat() + paddingTop + paddingBottom
+                )
+
+            }
+
+            isVerticalBottomAligned -> {
+                rectContent.set(
+                    0f,
+                    0f,
+                    maxContentWidth.toFloat() + paddingStart + paddingEnd,
+                    maxContentHeight.toFloat() + paddingTop + paddingBottom
+                )
+            }
+
+            else -> {
+                rectContent.set(
+                    0f,
+                    0f,
+                    maxContentWidth.toFloat() + paddingStart + paddingEnd,
+                    maxContentHeight.toFloat() + paddingTop + paddingBottom
+                )
+            }
+        }
+
+
+        rectBubble.set(0f, 0f, desiredWidth.toFloat(), desiredHeight.toFloat())
+        return Pair(desiredWidth, desiredHeight)
+    }
+
+    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
+        super.onLayout(changed, left, top, right, bottom)
+        layoutChildren(modifier)
+    }
+
+    private fun layoutChildren(modifier: BubbleModifier) {
+
+        val alignment = modifier.arrowAlignment
+
+        val isHorizontalRightAligned = isHorizontalRightAligned(alignment)
+        val isHorizontalLeftAligned = isHorizontalLeftAligned(alignment)
+        val isVerticalBottomAligned = isVerticalBottomAligned(alignment)
+
+        when {
+
+            // Arrow on left side
+            isHorizontalLeftAligned -> {
+                for (i in 0..childCount) {
+                    val child: View? = getChildAt(i)
+                    child?.let { child ->
+                        child.layout(
+                            (paddingStart + modifier.arrowWidth).toInt(),
+                            paddingTop,
+                            (paddingStart + child.width + modifier.arrowWidth).toInt(),
+                            paddingTop + child.height
+                        )
+                    }
+                }
+            }
+
+            // Arrow on right side
+            isHorizontalRightAligned -> {
+
+                for (i in 0..childCount) {
+                    val child: View? = getChildAt(i)
+                    child?.let { child ->
+                        child.layout(
+                            paddingStart,
+                            paddingTop,
+                            paddingStart + child.width,
+                            paddingTop + child.height
+                        )
+                    }
+                }
+            }
+
+            // Arrow at the bottom
+            isVerticalBottomAligned -> {
+                for (i in 0..childCount) {
+                    val child: View? = getChildAt(i)
+                    child?.let { child ->
+                        child.layout(
+                            paddingStart,
+                            paddingTop,
+                            paddingStart + child.width,
+                            (paddingTop + child.height + modifier.arrowHeight).toInt()
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        // Get path for this bubble
+        getBubbleClipPath(
+            path,
+            modifier = modifier,
+            contentRect = rectContent,
+        )
+
+        canvas.drawPath(path, paint)
+
+        if (isDebug) {
+            paintDebug.color = Color.RED
+            canvas.drawRect(rectBubble, paintDebug)
+            paintDebug.color = Color.BLUE
+            canvas.drawRect(rectContent, paintDebug)
+        }
+
+        if (modifier.shadowStyle == ShadowStyle.ELEVATION) {
+            outlineProvider = outlineProvider
+        }
+    }
+
+    fun update(modifier: BubbleModifier) {
+        this.modifier = modifier
+        paint.color = modifier.backgroundColor
+        invalidate()
+    }
+
+    override fun getOutlineProvider(): ViewOutlineProvider? {
+        return object : ViewOutlineProvider() {
+            override fun getOutline(view: View, outline: Outline) {
+                try {
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+                        outline.setConvexPath(path)
+
+                    } else {
+                        outline.setPath(path)
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/smarttoolfactory/bubblelayout/MainActivity.kt
@@ -0,0 +1,206 @@
+package com.smarttoolfactory.bubblelayout
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        /*
+            Alternative 1- Draw bubbles using Modifier programmatically
+         */
+//        setContentView(R.layout.activity_main)
+//        drawBubbles()
+
+        /*
+            Alternative 2- Draw bubbles with XML properties
+         */
+        setContentView(R.layout.activity_main_bubble_linear_with_attrs)
+    }
+
+    private fun drawBubbles() {
+
+        val dateBubble = findViewById<BubbleLayout>(R.id.bubbleDate)
+        dateBubble.modifier.apply {
+            arrowAlignment = NONE
+            backgroundColor = Color.rgb(212, 234, 244)
+        }
+        dateBubble.update(modifier = dateBubble.modifier)
+
+        dateBubble.paddingStart
+
+        /*
+            Sent bubble
+         */
+        val senderBubble1 = findViewById<BubbleLayout>(R.id.bubbleViewSender)
+        senderBubble1.modifier.apply {
+            arrowAlignment = RIGHT_TOP
+            arrowOffsetY = dp(5f)
+        }
+
+        senderBubble1.update(modifier = senderBubble1.modifier)
+
+
+        val senderBubble2 = findViewById<BubbleLayout>(R.id.bubbleViewSender2)
+        senderBubble2.modifier.apply {
+            arrowAlignment = RIGHT_BOTTOM
+        }
+
+        senderBubble2.update(modifier = senderBubble2.modifier)
+
+        val senderBubble3 = findViewById<BubbleLayout>(R.id.bubbleViewSender3)
+        senderBubble3.modifier.apply {
+            arrowAlignment = RIGHT_BOTTOM
+            withArrow = false
+        }
+
+        senderBubble3.update(modifier = senderBubble3.modifier)
+
+
+        val senderBubble4 = findViewById<BubbleLayout>(R.id.bubbleViewSender4)
+        senderBubble4.modifier.apply {
+            arrowAlignment = RIGHT_CENTER
+            arrowShape = ArrowShape.TRIANGLE_ISOSCELES
+        }
+
+        senderBubble4.update(modifier = senderBubble4.modifier)
+
+        /*
+            Received Bubbles
+         */
+        val receiverBubble = findViewById<BubbleLayout>(R.id.bubbleViewReceiver)
+
+        val modifierR1 = receiverBubble.modifier.apply {
+            backgroundColor = Color.WHITE
+            arrowAlignment = LEFT_BOTTOM
+            arrowOffsetY = -dp(5f)
+        }
+
+        receiverBubble.update(modifierR1)
+
+        val receiverBubble2 = findViewById<BubbleLayout>(R.id.bubbleViewReceiver2)
+
+        val modifierR2 = receiverBubble2.modifier.apply {
+            backgroundColor = Color.WHITE
+            arrowAlignment = LEFT_TOP
+        }
+
+        receiverBubble2.update(modifierR2)
+
+
+        val receiverBubble3 = findViewById<BubbleLayout>(R.id.bubbleViewReceiver3)
+
+        val modifierR3 = receiverBubble3.modifier.apply {
+            backgroundColor = Color.WHITE
+            arrowAlignment = LEFT_TOP
+            withArrow = false
+        }
+
+        receiverBubble3.update(modifierR3)
+
+
+        val receiverBubble4 = findViewById<BubbleLayout>(R.id.bubbleViewReceiver4)
+
+        val modifierR4 = receiverBubble4.modifier.apply {
+            arrowHeight = 24f.dp
+            backgroundColor = Color.WHITE
+            arrowAlignment = LEFT_CENTER
+            arrowShape = ArrowShape.TRIANGLE_ISOSCELES
+        }
+
+        receiverBubble4.update(modifierR4)
+
+
+        /*
+            Bottom Bubbles
+         */
+        val bubbleBottom1 = findViewById<BubbleLayout>(R.id.bubbleViewBottom1)
+
+        val modifierBottom1 = bubbleBottom1.modifier.apply {
+            backgroundColor = Color.rgb(251, 192, 45)
+            arrowAlignment = BOTTOM_CENTER
+            arrowShape = ArrowShape.TRIANGLE_ISOSCELES
+
+        }
+        bubbleBottom1.update(modifierBottom1)
+
+        val bubbleBottom2 = findViewById<BubbleLayout>(R.id.bubbleViewBottom2)
+        val modifierBottom2 = bubbleBottom2.modifier.apply {
+            backgroundColor = Color.rgb(142, 36, 170)
+            arrowAlignment = BOTTOM_LEFT
+            arrowShape = ArrowShape.TRIANGLE_RIGHT
+            arrowWidth = dp(48f)
+            arrowHeight = dp(16f)
+        }
+        bubbleBottom2.update(modifierBottom2)
+
+        val bubbleBottom3 = findViewById<BubbleLayout>(R.id.bubbleViewBottom3)
+        val modifierBottom3 = bubbleBottom3.modifier.apply {
+            backgroundColor = Color.rgb(0, 121, 107)
+            arrowAlignment = BOTTOM_RIGHT
+            arrowShape = ArrowShape.TRIANGLE_RIGHT
+        }
+        bubbleBottom3.update(modifierBottom3)
+
+
+        /*
+            Custom corner radius
+         */
+
+        val bubbleCustomRad1 = findViewById<BubbleLayout>(R.id.bubbleCustomRad1)
+
+        val modifierCR1 = bubbleCustomRad1.modifier.apply {
+            backgroundColor = Color.rgb(92, 107, 192)
+            arrowAlignment = LEFT_TOP
+            withArrow = false
+            cornerRadiusBundle = CornerRadius(
+                topLeft = dp(24f),
+                topRight = dp(16f),
+                bottomLeft = dp(2f),
+                bottomRight = dp(16f)
+            )
+        }
+
+        bubbleCustomRad1.update(modifierCR1)
+
+        val bubbleCustomRad2 = findViewById<BubbleLayout>(R.id.bubbleCustomRad2)
+
+        val modifierCR2 = bubbleCustomRad2.modifier.apply {
+            backgroundColor = Color.rgb(92, 107, 192)
+            arrowAlignment = LEFT_TOP
+            withArrow = false
+
+            cornerRadiusBundle = CornerRadius(
+                topLeft = dp(2f),
+                topRight = dp(16f),
+                bottomLeft = dp(2f),
+                bottomRight = dp(16f)
+            )
+        }
+        bubbleCustomRad2.update(modifierCR2)
+
+        val bubbleCustomRad3 = findViewById<BubbleLayout>(R.id.bubbleCustomRad3)
+        val modifierCR3 = bubbleCustomRad3.modifier.apply {
+            backgroundColor = Color.rgb(92, 107, 192)
+            arrowAlignment = LEFT_TOP
+            withArrow = false
+            cornerRadiusBundle = CornerRadius(
+                topLeft = dp(2f),
+                topRight = dp(16f),
+                bottomLeft = dp(8f),
+                bottomRight = dp(16f)
+            )
+        }
+        bubbleCustomRad3.update(modifierCR3)
+
+    }
+
+    private val Float.dp
+        get() = this@MainActivity.dp(this)
+
+    private val Int.dp
+        get() = this@MainActivity.dp(this)
+}
